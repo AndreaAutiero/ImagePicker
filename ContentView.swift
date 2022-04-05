bc37a02f3d690ddd187e8dbd579378c88923184e
AndreaAutiero <94606371+AndreaAutiero@users.noreply.github.com>
2022-04-05 12:53:42 +0200

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct ContentView: View {
+  @State private var showPhotoSheet = false
+  @State private var image: UIImage? = nil
+
+  var body: some View {
+    VStack {
+      Button(action: { showPhotoSheet = true }) {
+        Label("Choose photo", systemImage: "photo.fill")
+      }
+      .fullScreenCover(isPresented: $showPhotoSheet) {
+        PhotoPicker(filter: .images, limit: 1) { results in
+          PhotoPicker.convertToUIImageArray(fromResults: results) { (imagesOrNil, errorOrNil) in
+            if let error = errorOrNil {
+              print(error)
+            }
+            if let images = imagesOrNil {
+              if let first = images.first {
+                print(first)
+                image = first
+              }
+            }
+          }
+        }
+        .edgesIgnoringSafeArea(.all)
+      }
+
+      if let image = image {
+        Image(uiImage: image)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(maxWidth: 200, maxHeight: 200)
+      }
+    }
+  }
+}
